55ee1db8219e1b3cef5bcba55d72f32116d48520
juan <juan@casbrasil.com.br>
2020-11-02 15:41:48 -0200

    coonfi host

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['estoque-equipamento.herokuapp.com/']
+ALLOWED_HOSTS = ['gestor-equipamentos.herokuapp.com']
+# https://gestor-equipamentos.herokuapp.com/ | https://git.heroku.com/gestor-equipamentos.git
 
 
 # Application definition
