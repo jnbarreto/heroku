8b5b9d2ad4102311053526f73b8bf2b021a5187d
juan <juan@casbrasil.com.br>
2020-11-03 19:18:33 -0200

    arrumando o caminho do dir

--- app/settings/base.py
+++ app/settings/base.py
@@ -17,7 +17,7 @@ from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 DISABLE_COLLECTSTATIC=1
