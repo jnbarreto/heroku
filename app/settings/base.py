9ec1d399bba691ea1134ddc6c59065f7ca4a8212
juan <juan@casbrasil.com.br>
2020-11-03 17:04:56 -0200

    desabilitando o collectstatic

--- app/settings/base.py
+++ app/settings/base.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
-
+DISABLE_COLLECTSTATIC=1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
