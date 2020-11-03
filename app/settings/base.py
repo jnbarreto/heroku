2c283df5db8fad085b5dc13c909dcfd23783a837
juan <juan@casbrasil.com.br>
2020-11-03 19:01:09 -0200

    base dir

--- app/settings/base.py
+++ app/settings/base.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from pathlib import Path
+# from pathlib import Path
 from decouple import config
 
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 DISABLE_COLLECTSTATIC=1
 # Quick-start development settings - unsuitable for production
